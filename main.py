206555f5b967d3656e2e5557a468c9549f2c22fd
Cristian Andres Camargo Giraldo <cristiancamargo95@gmail.com>
2017-09-08 13:57:04 -0500

    First map implementation.

--- main.py
+++ main.py
@@ -1,24 +1,44 @@
 import pygame
-from sys import stdin
+
 def main():
     """
     Main game function.
      in: none
      out: game display
     """
-
+    
     pygame.init()
-    # Display game screen
-    disp = pygame.display.set_mode([800,600])
     # Set game title
     pygame.display.set_caption("Pakman")
     # Initialize game clock
     timing = pygame.time.Clock()
-    #Load assets
-    pacman=pygame.transform.scale(pygame.image.load('Assets/pacman.png'),(25,25))
+
+    # Constants for map items
+    NONE  = 0
+    WALL = 1
+    PACM = 2
+    # Map representation
+    gameMap = [
+        [WALL, WALL, WALL, WALL, WALL],
+        [WALL, NONE, NONE, NONE, WALL],
+        [WALL, NONE, PACM, NONE, WALL],
+        [WALL, NONE, NONE, NONE, WALL],
+        [WALL, WALL, WALL, WALL, WALL]
+    ]
+    # Map dimensions
+    TILESIZE  = 50
+    MAPWIDTH  = 5
+    MAPHEIGHT = 5
+    # Load assets
+    pacman=pygame.transform.scale(pygame.image.load('Assets/pacman.png'),(TILESIZE,TILESIZE))
+
+    # Display game screen
+    disp = pygame.display.set_mode((MAPWIDTH*TILESIZE,MAPHEIGHT*TILESIZE))
     
     quit = 0
     while (quit != 1):
+
+        # Check for events
         for event in pygame.event.get():
             if event.type == pygame.QUIT: 
                 sys.exit()
@@ -26,7 +46,14 @@ def main():
                 if event.key == pygame.K_q:
                     quit = 1
 
-        disp.blit(pacman,(400,400))            
+        # Draw map
+        for row in range(MAPHEIGHT):
+            for column in range(MAPWIDTH):
+                if gameMap[row][column] == WALL:            
+                    pygame.draw.rect(disp, (0,0,255), (column*TILESIZE,row*TILESIZE,TILESIZE,TILESIZE))
+                elif gameMap[row][column] == PACM:
+                    disp.blit(pacman,(column*TILESIZE,row*TILESIZE))           
+
         pygame.display.flip()
 
 main()
