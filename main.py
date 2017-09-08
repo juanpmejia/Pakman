75a66e469045110d2f903e93c7f5c6e215b61164
Cristian Andres Camargo Giraldo <cristiancamargo95@gmail.com>
2017-09-08 11:59:31 -0500

    Cleaned up main function.

--- main.py
+++ main.py
@@ -2,24 +2,31 @@ import pygame
 from sys import stdin
 def main():
     """
-    Funcion que retorna la visualizacion de un nivel
-    Entradas: Ninguna
-    Salidas: Pantalla
+    Main game function.
+     in: none
+     out: game display
     """
-    quit=1
-    while (quit!=0):
-        
-        pygame.init() # inicializa todos los modulos de pygame
-        disp= pygame.display.set_mode([800,600])# retorna superficie
-        pygame.display.set_caption("Pakman") # titulo de la pantalla
-        timing = pygame.time.Clock() # Reloj que controla el tiempo (Evita desperdiciar recursos)
-        red = (255,100,100) # Variable de color  azul= 0,0,255 negro= 0,0,0 rojo=255,0,0 verde= 0, 255, 0  Blanco= 255, 255, 255
-        pac=pygame.image.load('Assets/pacman.png')
-        pacman=pygame.transform.scale(pac,(25,25))
-        disp.blit(pacman,(400,400))
-        pygame.display.update()
-        if (stdin.readline().strip()=="q"):
-            quit=0
 
-    #pygame.quit()
+    pygame.init()
+    # Display game screen
+    disp = pygame.display.set_mode([800,600])
+    # Set game title
+    pygame.display.set_caption("Pakman")
+    # Initialize game clock
+    timing = pygame.time.Clock()
+    #Load assets
+    pacman=pygame.transform.scale(pygame.image.load('Assets/pacman.png'),(25,25))
+    
+    quit = 0
+    while (quit != 1):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: 
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q:
+                    quit = 1
+
+        disp.blit(pacman,(400,400))            
+        pygame.display.flip()
+
 main()
