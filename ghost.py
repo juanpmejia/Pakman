58e6fe820bd3cc26ea7ce5c046d1c01aaf293fe1
Cristian Andres Camargo Giraldo <cristiancamargo95@gmail.com>
2017-09-16 05:34:49 -0500

    Enabled red ghost to chase pacman.

--- ghost.py
+++ ghost.py
@@ -22,3 +22,21 @@ class Ghost:
 	def move(self, x, y):
 		self.x += x
 		self.y += y
+
+class GhostRed(Ghost):
+	# Initialization
+	def __init__(self, x, y, surface, sprite, size):
+		Ghost.__init__(self, x, y, surface, sprite, size)
+		self.path = []
+
+	def setPath(self, path):
+		self.path = path
+		self.path.pop()
+
+	def followPath(self):
+		if len(self.path) > 1:
+			curr = self.getPos()
+			next = self.path.pop()
+			dirX = next[0]-curr[0]
+			dirY = next[1]-curr[1]
+			self.move(dirX, dirY)
--- main.py
+++ main.py
@@ -28,7 +28,7 @@ class Game():
 
         # Initialize players
         self.pacman = Pacman(1,1,self.disp,'Assets/pacman.png',TILESIZE)
-        self.ghostRed = Ghost(3,21,self.disp,'Assets/red.png',TILESIZE)
+        self.ghostRed = GhostRed(3,21,self.disp,'Assets/red.png',TILESIZE)
         self.ghostBlue = Ghost(8,6,self.disp,'Assets/blue.png',TILESIZE)
         self.ghostPink = Ghost(11,21,self.disp,'Assets/pink.png',TILESIZE)
         self.ghostOrange = Ghost(4,5,self.disp,'Assets/orange.png',TILESIZE)
@@ -101,12 +101,19 @@ class Game():
             currPos = self.ghostOrange.getPos()
             self.maze.setTile(ORNG, currPos[0], currPos[1])
 
+
     # Movement logic for the red ghost
     def moveRed(self):
         currPos = self.ghostRed.getPos()
         targetPos = self.pacman.getPos()
-        path = astar(currPos, targetPos, self.maze)
+        self.ghostRed.setPath(astar(currPos, targetPos, self.maze))
+        self.maze.setTile(NONE, currPos[0], currPos[1])
+        #print self.ghostRed.path
+        self.ghostRed.followPath()
+        currPos = self.ghostOrange.getPos()
+        self.maze.setTile(ORNG, currPos[0], currPos[1])
 
+    
     # Main game loop
     def mainLoop(self):
 
