77500c9c470f7da7fc0fcb11f79ff19e1cbc3d65
Cristian Andres Camargo Giraldo <cristiancamargo95@gmail.com>
2017-11-23 22:38:28 -0500

    Fixed some bugs.

--- level_2/main.py
+++ level_2/main.py
@@ -3,77 +3,61 @@ import pygame
 from pacman import *
 
 # Some constants
-DISPLAY_WIDTH = 800
 DISPLAY_HEIGHT = 600
+DISPLAY_WIDTH = 800
+COLOR_BLACK = (0,0,0)
 
 # Main game class
 class Game():
 
-    # Initialization
-    def __init__(self):
-        # Set game title
-        pygame.display.set_caption("Pakman")
-        # Initialize game variables
-        self.clock = pygame.time.Clock()
-        self.quit = 0
+	# Initialization
+	def __init__(self):
+		# Set game title
+		pygame.display.set_caption("Pakman")
+		# Initialize game variables
+		self.clock = pygame.time.Clock()
+		self.quit = 0
 
-        # Initialize game screen
-        self.screen = pygame.display.set_mode(DISPLAY_WIDTH,DISPLAY_HEIGHT)
+		# Initialize game screen
+		self.screen = pygame.display.set_mode([DISPLAY_WIDTH,DISPLAY_HEIGHT])
 
-        # Initialize players
-        self.pacman = Pacman('Assets/pacman.png')
+		# Initialize players
+		self.pacman = Pacman('../Assets/pacman.png', (50,50))
 
-        allsprites = pygame.sprite.Group()
-        allsprites.add(pacman)
+		self.allsprites = pygame.sprite.Group()
+		self.allsprites.add(self.pacman)
 
-        self.mainLoop()
+		self.mainLoop()
 
-    # Check for game events
-    def checkEvents(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:  
-                self.quit = 1
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_q:
-                    self.quit = 1
-                    
-    # Movement logic for the player
-    def movePlayer(self):
-        pressed = pygame.key.get_pressed()                     
-        dir = 0  
+	# Check for game events
+	def checkEvents(self):
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:  
+				self.quit = 1
+			elif event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_q:
+					self.quit = 1
+								
+	# Main game loop
+	def mainLoop(self):
 
-        if pressed[pygame.K_UP]:
-            dir = 270
-        elif pressed[pygame.K_DOWN]:
-            dir = 90
-        elif pressed[pygame.K_LEFT]:
-            dir = 180
-        elif pressed[pygame.K_RIGHT]:
-            dir = 0
-                
-    # Main game loop
-    def mainLoop(self):
+		while (self.quit != 1):
+			# Clear the screen
+			self.screen.fill(COLOR_BLACK)
 
-        while (self.quit != 1):
-            # Clear the screen
-            screen.fill(black)
+			# Cinematic 10 fps
+			self.clock.tick(30)
 
-            # Cinematic 10 fps
-            self.clock.tick(10)
+			self.checkEvents()
+			self.pacman.movePlayer()
+			self.allsprites.update()
+			
+			# Draw Everything
+			self.allsprites.draw(self.screen)
 
-            self.checkEvents()
-            pacman.update()
-            
-            # Draw Everything
-            allsprites.draw(screen)
-
-            # Flip the screen and show what we've drawn
-            pygame.display.flip()
-
-        text = font.render("Game Over", True, white)
-        textpos = text.get_rect(background.get_width()/2)
-        textpos.top = 300
-        screen.blit(text, textpos)
+			# Flip the screen and show what we've drawn
+			pygame.display.flip()
+			
 
 pygame.init()
 Game()
--- level_2/pacman.py
+++ level_2/pacman.py
@@ -1,29 +1,57 @@
 import pygame
 import math
 
+# Some constants
+DISPLAY_HEIGHT = 600
+DISPLAY_WIDTH = 800
+
 class Pacman(pygame.sprite.Sprite):
 
 	# Initialization
-	def __init__(self, image, x, y):
-		super().__init__
+	def __init__(self, image, size):
+		pygame.sprite.Sprite.__init__(self)
 
 		# Speed in pixels per cycle
 		self.speed = 10.0
 
 		# Direction (in degrees)
-    	direction = 0
+		self.direction = 90
 
-		self.image = pygame.image.load(image).convert()
+		self.image = pygame.transform.scale(pygame.image.load(image).convert_alpha(), size)
 		self.rect = self.image.get_rect()
-		self.rect.x = x
-		self.rect.y = y
+		self.rect.x = 0
+		self.rect.y = 0
 
 	# Get the character's position
-	def getPos(self)
+	def getPos(self):
 		return (self.rect.x, self.rect.y)
 
 	# Update the character's position
 	def update(self):
 		radians = math.radians(self.direction)
 		self.rect.x += self.speed * math.sin(radians)
-		self.rect.y -= self.speed * math.cos(radians)
+		self.rect.y -=	  self.speed * math.cos(radians)
+
+		if self.rect.left < 0:
+			self.rect.left = 0
+		elif self.rect.right > DISPLAY_WIDTH:
+			self.rect.right = DISPLAY_WIDTH
+
+		if self.rect.top < 0:
+			self.rect.top = 0
+		elif self.rect.bottom > DISPLAY_HEIGHT:
+			self.rect.bottom = DISPLAY_HEIGHT
+
+	# Movement logic for the player
+	def movePlayer(self):
+		pressed = pygame.key.get_pressed()   
+
+		if pressed[pygame.K_UP]:
+			self.direction = 0   
+		elif pressed[pygame.K_RIGHT]:
+			self.direction = 90
+		elif pressed[pygame.K_DOWN]:
+			self.direction = 180
+		elif pressed[pygame.K_LEFT]:
+			self.direction = 270
+		
